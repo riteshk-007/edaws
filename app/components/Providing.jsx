02c27823b749f745d4c4fef435b9f10053db90a4
riteshk-007 <codeshorts007@gmail.com>
2024-04-16 14:18:49 +0530

    Add Providing component to Home page

---
+++ app/components/Providing.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Providing = () => {
+  return (
+    <div className="bg-blue-100 p-8 rounded-lg shadow-lg">
+      <h2 className="text-3xl font-bold mb-4 text-blue-700">
+        Empowering India&apos;s Underprivileged Children Through Education
+      </h2>
+      <p className="text-lg text-gray-800">
+        Our mission is to empower underprivileged children across India by
+        providing them with quality education. Your generous donation can light
+        the lamp of knowledge in their lives, helping them break free from the
+        shackles of poverty. Your contribution will not only fund their
+        education but also pave the way for their brighter and prosperous
+        future. Join us in this noble cause and make a difference today.
+      </p>
+    </div>
+  );
+};
+
+export default Providing;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import { ImgCard } from "./components/ImgCard";
+import Providing from "./components/Providing";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ImgCard />
+      <Providing />
     </>
   );
 }
