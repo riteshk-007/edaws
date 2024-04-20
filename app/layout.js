526f93dab833ddc55d79156eb862780b17449801
Riteshk_007 <codeshorts007@gmail.com>
2024-04-20 21:03:43 +0530

    Update default title and description in layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,10 +9,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
-    default: "edaws - Educational development And Agriculture Welfare Society",
+    default: "EDAWS - Educational development And Agriculture Welfare Society",
   },
   description:
-    "edaws - Educational development And Agriculture Welfare Society is a non-profit organization that works for the welfare of the society.",
+    "EDAWS - Educational development And Agriculture Welfare Society is a non-profit organization that works for the welfare of the society.",
 };
 
 export default function RootLayout({ children }) {
