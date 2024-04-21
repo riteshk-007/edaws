d13003c86e06708a02f5b35424479e7719c62801
Riteshk_007 <codeshorts007@gmail.com>
2024-04-21 17:41:26 +0530

    Update About Page Details

--- app/(pages)/about/page.js
+++ app/(pages)/about/page.js
@@ -16,8 +16,11 @@ const About = () => {
           EDAWS - Educational development And Agriculture Welfare Society
         </h1>
       </div>
-      <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-center my-10">
+      <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-center my-5 md:my-10">
         WHO WE ARE
+      </h1>
+      <h1 className="text-2xl md:text-4xl font-bold text-center my-5 md:my-10">
+        Edaws - Educational Development And Agriculture Welfare Society
       </h1>
       <div className="flex flex-col md:flex-row w-full items-center justify-center">
         <div className="flex items-center justify-center w-full md:w-1/2 p-3">
@@ -29,53 +32,81 @@ const About = () => {
             className="object-cover rounded-md  shadow-2xl"
           />
         </div>
-        <div className="flex items-center justify-center w-full md:w-1/2 p-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
-          excepturi a omnis, doloribus inventore placeat quod consequuntur,
-          ullam nam temporibus porro modi provident nemo nisi explicabo maiores
-          aperiam accusamus labore tenetur ipsum suscipit aut laudantium ex?
-          Libero culpa cum laboriosam qui nisi consectetur, ad placeat, quos
-          accusamus, expedita iste assumenda? Nemo ut facere praesentium, quos
-          quod error veniam neque laboriosam voluptate, provident atque
-          nesciunt! Nihil similique, itaque dolor veritatis quo est molestias
-          ratione reiciendis officia voluptatem molestiae quam corrupti beatae!
-          Culpa consectetur eum minus earum, nobis nisi numquam quod, cum quasi
-          aliquam autem atque exercitationem ratione animi doloribus sed iste
-          adipisci commodi. Possimus asperiores atque itaque. At dolore in
-          accusamus aut beatae deserunt, aspernatur recusandae rem ducimus
-          laborum nisi ex omnis aliquam provident fuga. Voluptate vero ut
-          officiis earum perspiciatis laborum cum minima aspernatur corrupti
-          beatae. Dolor quibusdam eos perferendis expedita, eligendi similique
-          voluptas eum reprehenderit repellat. Provident, eum! Nobis nemo labore
-          aperiam nam? Tempora adipisci quam perferendis officiis inventore
-          quidem temporibus neque molestias beatae iusto aperiam, ad error
-          impedit alias sequi praesentium, similique sapiente voluptatem aliquid
-          ratione magnam cum.
+
+        <div className="flex items-start justify-center w-full md:w-1/2 p-3 flex-col">
+          <h2 className="text-2xl font-semibold my-1 text-black">
+            Company&apos;s Journey
+          </h2>
+          <p className="text-md text-gray-800">
+            Edaws - Educational Development And Agriculture Welfare Society, was
+            founded with a vision to uplift the underprivileged and uneducated
+            families by providing them with the tools and resources to excel in
+            education and agricultural practices. Our journey began with the aim
+            to address the lack of awareness and guidance that hindered the
+            social and economic progress of many communities.
+          </p>
+          <h2 className="text-2xl font-semibold my-1 text-black">
+            Purpose and Goals
+          </h2>
+          <p className="text-md text-gray-800">
+            Our primary goal at Edaws is to promote educational development and
+            modernize agriculture in order to create employment opportunities
+            for individuals in society. We strive to remove social malpractices
+            by empowering families below the poverty line with education and
+            encourage them to reach their full potential. Our focus is on
+            guiding individuals towards meaningful and sustainable paths that
+            align with their abilities and interests, ultimately breaking the
+            cycle of poverty.
+          </p>
+          <h2 className="text-2xl font-semibold my-1 text-black">
+            Introduction to the Team
+          </h2>
+          <p className="text-md text-gray-800">
+            Our team at Edaws is comprised of dedicated professionals who are
+            passionate about making a positive impact in the community. From
+            educators to agricultural experts, each member brings a unique set
+            of skills and experiences that drive our mission forward. Together,
+            we work tirelessly to support and guide individuals towards a
+            brighter future.
+          </p>
         </div>
       </div>
       <div className="flex flex-col-reverse md:flex-row w-full items-center justify-center">
-        <div className="flex items-center justify-center w-full md:w-1/2 p-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
-          excepturi a omnis, doloribus inventore placeat quod consequuntur,
-          ullam nam temporibus porro modi provident nemo nisi explicabo maiores
-          aperiam accusamus labore tenetur ipsum suscipit aut laudantium ex?
-          Libero culpa cum laboriosam qui nisi consectetur, ad placeat, quos
-          accusamus, expedita iste assumenda? Nemo ut facere praesentium, quos
-          quod error veniam neque laboriosam voluptate, provident atque
-          nesciunt! Nihil similique, itaque dolor veritatis quo est molestias
-          ratione reiciendis officia voluptatem molestiae quam corrupti beatae!
-          Culpa consectetur eum minus earum, nobis nisi numquam quod, cum quasi
-          aliquam autem atque exercitationem ratione animi doloribus sed iste
-          adipisci commodi. Possimus asperiores atque itaque. At dolore in
-          accusamus aut beatae deserunt, aspernatur recusandae rem ducimus
-          laborum nisi ex omnis aliquam provident fuga. Voluptate vero ut
-          officiis earum perspiciatis laborum cum minima aspernatur corrupti
-          beatae. Dolor quibusdam eos perferendis expedita, eligendi similique
-          voluptas eum reprehenderit repellat. Provident, eum! Nobis nemo labore
-          aperiam nam? Tempora adipisci quam perferendis officiis inventore
-          quidem temporibus neque molestias beatae iusto aperiam, ad error
-          impedit alias sequi praesentium, similique sapiente voluptatem aliquid
-          ratione magnam cum.
+        <div className="flex items-start flex-col justify-center w-full md:w-1/2 p-3">
+          <h2 className="text-2xl font-semibold my-1 text-black">Offerings</h2>
+          <p className="text-md text-gray-800">
+            At Edaws, we offer a wide range of educational and agricultural
+            programs designed to meet the diverse needs of our beneficiaries.
+            From literacy classes and vocational training to sustainable farming
+            techniques, we provide comprehensive support to help individuals
+            build a foundation for success. Additionally, our team offers
+            personalized guidance and mentorship to ensure that each individual
+            reaches their full potential.
+          </p>
+          <h2 className="text-2xl font-semibold my-1 text-black">
+            {" "}
+            Achievements
+          </h2>
+          <p className="text-md text-gray-800">
+            Over the years, Edaws has made significant strides in promoting
+            education and modernizing agriculture within the community. We have
+            successfully helped numerous individuals break the cycle of poverty
+            and achieve economic independence. Our dedication to providing
+            awareness and guidance has resulted in tangible improvements in the
+            lives of many families, making a lasting impact on the community.
+          </p>
+          <h2 className="text-2xl font-semibold my-1 text-black">
+            Call to Action
+          </h2>
+          <p className="text-md text-gray-800">
+            Join us in our mission to empower individuals through education and
+            agriculture. Whether you are looking for support in your academic
+            journey or seeking guidance in agricultural practices, Edaws is here
+            to help. Together, we can create a brighter and more sustainable
+            future for all members of society. Contact us today to learn more
+            about our programs and how you can get involved. Let&apos;s make a
+            difference together!
+          </p>
         </div>
         <div className="flex items-center justify-center w-full md:w-1/2 p-3 mt-5">
           <Image
